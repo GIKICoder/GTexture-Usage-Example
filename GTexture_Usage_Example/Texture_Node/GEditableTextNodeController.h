3558b03119a9d43655fbcb33d9d3f81ec9fe967b
GIKI <810373457@qq.com>
2018-12-23 21:16:44 +0800

    commit: update editableText node

---
+++ GTexture_Usage_Example/Texture_Node/GEditableTextNodeController.h
@@ -0,0 +1,17 @@
+//
+//  GEditableTextNodeController.h
+//  GTexture_Usage_Example
+//
+//  Created by GIKI on 2018/12/23.
+//  Copyright © 2018 GIKI. All rights reserved.
+//
+
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GEditableTextNodeController : ASViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GTexture_Usage_Example/Texture_Node/GEditableTextNodeController.m
@@ -0,0 +1,32 @@
+//
+//  GEditableTextNodeController.m
+//  GTexture_Usage_Example
+//
+//  Created by GIKI on 2018/12/23.
+//  Copyright © 2018 GIKI. All rights reserved.
+//
+
+#import "GEditableTextNodeController.h"
+
+@interface GEditableTextNodeController ()
+@property (nonatomic, strong) ASEditableTextNode * editableTextNode;
+@end
+
+@implementation GEditableTextNodeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
